e600d6739db953cf248ed6ebacb224404e2fc597
Mdjurdj1 <github email address>
2017-05-06 15:17:03 -0400

    Added a POST api v1 users route

---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Users", type: :request do
+
+  describe "POST /users" do
+
+    describe "on success" do
+
+      it "creates a user from the params" do
+        params = { user: {
+          email: 'mdjurdj1@gmail.com',
+          password: 'password'
+          }
+        }
+
+        post "/api/v1/users",
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(body['token']).not_to eq(nil)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['email']).not_to eq('mdjurdj1@gmail.com')
+        expect(body['user']['password_digest']).to eq(nil)
+      end
+
+      it "returns the newly created user, and a JWT token"
+    end
+
+    describe "on error" do
+
+      it "requires a valid email and password" do
+      end
+    end
+  end
+
+end
