88fda36abd88af273938d406d4a7c825f1033b34
Mdjurdj1 <github email address>
2017-05-06 17:17:08 -0400

    Added route for post/api/v1/auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+
+      #/api/v1/users
       resources :users, only: [:create]
+
+      #api/v1/auth
+      post '/auth', to: "auth#login"
     end
   end
 end
