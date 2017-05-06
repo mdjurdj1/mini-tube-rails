7c43cc2d9efeac7ca289a83eaf543eb8abd99512
Mdjurdj1 <github email address>
2017-05-06 17:03:13 -0400

    Added and passed tests for return value of post api/v1/users

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe "Api::V1::Users", type: :request do
 
     describe "on success" do
 
-      it "creates a user from the params" do
-        params = { user: {
-          email: 'mdjurdj1@gmail.com',
-          password: 'password'
+      before(:each) do
+        params = {
+          user: {
+            email: 'mdjurdj1@gmail.com',
+            password: 'password'
           }
         }
 
@@ -17,22 +18,46 @@ RSpec.describe "Api::V1::Users", type: :request do
           params: params.to_json,
           headers: { 'Content-Type': 'application/json' }
 
-        body = JSON.parse(response.body)
-        expect(response.status).to eq(200)
-        expect(body['token']).not_to eq(nil)
+        @response = response
+      end
+
+      it "creates a user from the params" do
+        expect(User.all.count).to eq(1)
+      end
+
+      it "returns the newly created user, and a JWT token" do
+        body = JSON.parse(@response.body)
+
+        expect(@response.status).to eq(200)
         expect(body['user']['id']).not_to eq(nil)
         expect(body['user']['email']).to eq('mdjurdj1@gmail.com')
         expect(body['user']['password_digest']).to eq(nil)
+        expect(body['token']).not_to eq(nil)
       end
-
-      it "returns the newly created user, and a JWT token"
     end
 
-    describe "on error" do
+      describe "on error" do
 
       it "requires a valid email and password" do
+        params = {
+          user: {
+            email: '',
+            password: ''
+          }
+        }
+
+        post "/api/v1/users",
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+
+          body = JSON.parse(response.body)
+
+          expect(response.status).to eq(500)
+          expect(body["errors"]).to eq({
+            "password"=>["can't be blank"],
+            "email"=>["can't be blank", "is invalid"]
+            })
       end
     end
   end
-
 end
