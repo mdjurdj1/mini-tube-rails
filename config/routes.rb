3a33345dd8f9d9a6d4e60152ca18df8734d084b0
Mdjurdj1 <github email address>
2017-05-06 17:42:41 -0400

    Add post api/v1/auth refresh route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
       #api/v1/auth
       post '/auth', to: "auth#login"
+      post '/auth/refresh', to: "auth#refresh"
     end
   end
 end
--- spec/requests/api/v1/auth_request_spec.rb
+++ spec/requests/api/v1/auth_request_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
 
+      before(:each) do
+        @user = User.create(
+          email: 'mdjurdj1@gmail.com',
+          password: 'password'
+        )
+    end
+
+
   describe "POST /auth" do
 
-    before(:each) do
-      User.create(
-        email: 'mdjurdj1@gmail.com',
-        password: 'password'
-      )
-  end
-  
     describe "on success" do
 
       before(:each) do
@@ -82,5 +83,78 @@ RSpec.describe "Api::V1::Users", type: :request do
             })
       end
     end
+  end
+
+  describe "POST /auth/refresh" do
+
+    describe "on success" do
+
+      before(:each) do
+        token = Auth.create_token(@user.id)
+
+        post "/api/v1/auth/refresh",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer: #{token}"
+          }
+
+        @response = response
+      end
+
+
+      it "returns an existing user (from headers JWT), and a new JWT token" do
+        body = JSON.parse(@response.body)
+
+        expect(@response.status).to eq(200)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['email']).to eq('mdjurdj1@gmail.com')
+        expect(body['user']['password_digest']).to eq(nil)
+        expect(body['token']).not_to eq(nil)
+      end
+    end
+
+    #   describe "on error" do
+    #
+    #   it "unable to find user with email" do
+    #     params = {
+    #       user: {
+    #         email: 'mike@ayylmao.com',
+    #         password: 'password'
+    #       }
+    #     }
+    #
+    #     post "/api/v1/auth",
+    #       params: params.to_json,
+    #       headers: { 'Content-Type': 'application/json' }
+    #
+    #       body = JSON.parse(response.body)
+    #
+    #       expect(response.status).to eq(500)
+    #       expect(body["errors"]).to eq({
+    #         "email"=>["Unable to find a user with the provided email address"]
+    #         })
+    #   end
+    #
+    #   it 'password doesnt match with the provided email' do
+    #     params = {
+    #       user: {
+    #         email: 'mdjurdj1@gmail.com',
+    #         password: 'wronggg'
+    #       }
+    #     }
+    #
+    #     post "/api/v1/auth",
+    #       params: params.to_json,
+    #       headers: { 'Content-Type': 'application/json' }
+    #
+    #       body = JSON.parse(response.body)
+    #
+    #       expect(response.status).to eq(500)
+    #       expect(body["errors"]).to eq({
+    #         "password"=>["Password does not match the provided email"]
+    #         })
+    #   end
+    # end
+
   end
 end
