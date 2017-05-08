e7ffe515ff06f118c7b42211a86cd660352f7341
Mdjurdj1 <github email address>
2017-05-08 18:30:42 -0400

    Added create action to playlists controller

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,9 +1,27 @@
 class Api::V1::PlaylistsController < ApplicationController
-  before_action :authenticate_token!, only: [:show, :update]
+  before_action :authenticate_token!, only: [:show, :create, :update]
+
+  def create
+    @playlist = current_user.playlists.new(playlist_params)
+    if @playlist.save
+      render 'playlists/playlist.json.jbuilder', playlists: @playlist
+    else
+      render json: {
+        errors: @playlist.errors
+      }, status: 500
+    end
+  end
 
   def show
   end
 
   def update
-  end 
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :description)
+  end
+  
 end
