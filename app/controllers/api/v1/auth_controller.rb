e8866208b504bcf14e84a3a1187bc7d9009da12b
Mdjurdj1 <github email address>
2017-05-08 13:18:28 -0400

    Changed login search to find by username params instead of emails

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,15 +2,17 @@ class Api::V1::AuthController < ApplicationController
   before_action :authenticate_token!, only: [:refresh]
 
   def login
-    @user = User.find_by(email: params[:user][:email])
+    @user = User.find_by(email: params[:user][:username])
     if !@user
       render json: {
         errors: {
           email: ["Unable to find a user with the provided email address"]
         }
       }, status: 500
+
     elsif @user && @user.authenticate(params[:user][:password])
       render 'users/user_with_token.json.jbuilder', user: @user
+
     else
        render json: {
          errors: {
